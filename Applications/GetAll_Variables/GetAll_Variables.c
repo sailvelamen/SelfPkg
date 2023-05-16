d3e43d4bedf9b3e70e018c9593386a1b771aa219
fanjixintel <fanx.ji@intel.com>
2023-05-16 10:38:45 +0800

    update GetAll_Variables

--- Applications/GetAll_Variables/GetAll_Variables.c
+++ Applications/GetAll_Variables/GetAll_Variables.c
@@ -56,6 +56,20 @@ int main(IN int Argc, IN char **Argv)
     UINT32 Atts;
 //    BOOLEAN write;
 
+    printf("------------------------------SetVariable-----------------------------\n");
+    CHAR16 * FoundVarNameTest = (CHAR16*)L"TestVariable";
+    //E8597DEF-C5EF-4C07-860C-EA864553F186
+    EFI_GUID TestGuid = { 0xE8597DEF, 0xC5EF, 0x4C07, { 0x86, 0x0C, 0xEA, 0x86, 0x45, 0x53, 0xF1, 0x86 }};
+    UINT8 DataBufferTest[] = {1, 2, 3, 4, 5};
+    UINTN DataSizeTest = sizeof(DataBufferTest) / sizeof(DataBufferTest[0]);
+    Atts = EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS;
+    Status = gRT->SetVariable(FoundVarNameTest, &TestGuid, Atts, DataSizeTest, DataBufferTest);
+    Print(L"Variable: %-22s", FoundVarNameTest);
+    printf(" Attributes: 0x%08x  DataSizeTest: 0x%03x  \n", Atts, DataSizeTest);
+    Print (L"GUID: ");
+    PrintGuid(&TestGuid);
+    PrintDataBuffer(DataBufferTest, DataSizeTest);
+
     printf("\n------------------------------GetAllVariables-----------------------------\n");
     EFI_GUID FoundVarGuid;
     while(1)
@@ -81,7 +95,7 @@ int main(IN int Argc, IN char **Argv)
         }
         if(Status == EFI_NOT_FOUND)
             break;
-        Print(L"Variable:%-22s", FoundVarName);
+        Print(L"Variable: %-22s", FoundVarName);
         DataSize = DataBufferSize;
         Status = gRT->GetVariable(FoundVarName, &FoundVarGuid, &Atts, &DataSize, DataBuffer);
         if(Status == EFI_BUFFER_TOO_SMALL)
@@ -101,20 +115,15 @@ int main(IN int Argc, IN char **Argv)
             NameSize = NameBufferSize;
             Status = gRT->GetVariable(FoundVarName, &FoundVarGuid, &Atts, &DataSize, DataBuffer);
         }
-        printf(" Attributes:0x%08x  DataSize: 0x%03x  \n", Atts, DataSize);
-        Print (L"GUID:");
+        printf(" Attributes: 0x%08x  DataSize: 0x%03x  \n", Atts, DataSize);
+        Print (L"GUID: ");
         PrintGuid(&FoundVarGuid);
-
-        // Status = gRT->SetVariable(FoundVarName, &FoundVarGuid, Atts, DataSize, DataBuffer);
-        // if(Status == EFI_SECURITY_VIOLATION)
-        //     write = FALSE;
-        // (write == FALSE) ? printf("False\n") : printf("True\n");
     }//End while(1)
 
 
     printf("\n------------------------------GetVariable-----------------------------\n");
-    FoundVarName = (CHAR16*)L"ConIn";
-    EFI_GUID *Guid = &gEfiGlobalVariableGuid;
+    FoundVarName = (CHAR16*)L"TestVariable";
+    EFI_GUID *Guid = &TestGuid;
     Status = gRT->GetVariable(FoundVarName, Guid, &Atts, &DataSize, DataBuffer);
     if(Status == EFI_BUFFER_TOO_SMALL)
     {
@@ -134,26 +143,11 @@ int main(IN int Argc, IN char **Argv)
     DataBuffer[0] = 1;
     gRT->SetVariable(FoundVarName, Guid, Atts, DataSize, DataBuffer);
 
-    Print(L"Variable:%-22s", FoundVarName);
-    printf(" Attributes:0x%08x  DataSize: 0x%03x  \n", Atts, DataSize);
-    printf("GUID: ");
+    Print(L"Variable: %-22s", FoundVarName);
+    printf(" Attributes: 0x%08x  DataSize: 0x%03x  \n", Atts, DataSize);
+    Print (L"GUID: ");
     PrintGuid(Guid);
     PrintDataBuffer(DataBuffer, DataSize);
-
-    // printf("------------------------------SetVariable-----------------------------\n");
-    // FoundVarName = (CHAR16*)L"TestVariable";
-    // //E8597DEF-C5EF-4C07-860C-EA864553F186
-    // EFI_GUID TestGuid = { 0xE8597DEF, 0xC5EF, 0x4C07, { 0x86, 0x0C, 0xEA, 0x86, 0x45, 0x53, 0xF1, 0x86 }};
-    // DataSize = 5;
-    // DataBuffer = AllocatePool(DataBufferSize);
-    // DataBuffer[0] = 1;DataBuffer[1] = 2;DataBuffer[2] = 3;DataBuffer[3] = 4;DataBuffer[4] = 5;
-    // Atts = EFI_VARIABLE_APPEND_WRITE;
-    // Status = gRT->SetVariable(FoundVarName, &TestGuid, Atts, DataSize, DataBuffer);
-    // Print(L"Variable:%-22s", FoundVarName);
-    // printf(" Attributes:0x%08x  DataSize: 0x%03x  \n", Atts, DataSize);
-    // printf("GUID: ");
-    // PrintGuid(&TestGuid);
-    // PrintDataBuffer(DataBuffer, DataSize);
 
     return 0;
 }

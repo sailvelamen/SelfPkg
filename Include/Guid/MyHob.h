50538623bba7859d9429830779c854d234609c44
sailvelamen <jifan1914346052@qq.com>
2024-04-14 18:12:12 +0800

    Update gcc build error

--- Include/Guid/MyHob.h
+++ Include/Guid/MyHob.h
@@ -5,7 +5,7 @@
 
 #define EFI_SELF_HOB_GUID { 0x3E0481ED, 0xF593, 0x6976, { 0x43, 0xD9, 0x06, 0x10, 0x0E, 0xE4, 0x34, 0xA0 }}
 
-EFI_GUID  gEfiSelfHobGuid;
+// EFI_GUID  gEfiSelfHobGuid;
 
 typedef struct {        
   UINT16    ID;
